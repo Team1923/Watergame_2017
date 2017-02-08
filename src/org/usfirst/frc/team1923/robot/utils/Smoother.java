530bd83fb589fd06883738aa8e15c89077ebabc7
Peter Wu <ilikepie.wu307@gmail.com>
2017-02-07 19:57:04 -0500

    fixed formatting again

--- src/org/usfirst/frc/team1923/robot/utils/Smoother.java
+++ src/org/usfirst/frc/team1923/robot/utils/Smoother.java
@@ -4,11 +4,13 @@ import org.usfirst.frc.team1923.robot.RobotMap;
 
 /**
  * Based off of Chris's Calculator.java.
+ * 
  */
 public class Smoother {
 
 	public static double ease(double current, double old, double coalesceIncrement) {
 		double new;
+		
 		if (current < old - coalesceIncrement) {
 			new = old - coalesceIncrement;
 		} 
@@ -18,6 +20,7 @@ public class Smoother {
 		else {
 			new = current;
 		}
+		
 		return new;
 	}
 	
