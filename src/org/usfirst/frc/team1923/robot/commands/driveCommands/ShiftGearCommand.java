44805cf62273cbebbb37ea4436a52c01e3dd243d
Tim Magoun <timmagoun@gmail.com>
2017-02-15 18:57:14 -0500

    Merge commit pt.2

--- src/org/usfirst/frc/team1923/robot/commands/driveCommands/ShiftGearCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/driveCommands/ShiftGearCommand.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/org/usfirst/frc/team1923/robot/commands/ShiftGearCommand.java
-package org.usfirst.frc.team1923.robot.commands;
-=======
 package org.usfirst.frc.team1923.robot.commands.driveCommands;
->>>>>>> beta:src/org/usfirst/frc/team1923/robot/commands/driveCommands/ShiftGearCommand.java
 
 import org.usfirst.frc.team1923.robot.Robot;
 
@@ -26,15 +22,9 @@ public class ShiftGearCommand extends InstantCommand {
 	// Called once when the command executes
 	protected void initialize() {
 		if (up) {
-<<<<<<< HEAD:src/org/usfirst/frc/team1923/robot/commands/ShiftGearCommand.java
-			Robot.driveSubSystem.shiftUp();
-		} else {
-			Robot.driveSubSystem.shiftDown();
-=======
 			Robot.driveSubSys.shiftUp();
 		} else {
 			Robot.driveSubSys.shiftDown();
->>>>>>> beta:src/org/usfirst/frc/team1923/robot/commands/driveCommands/ShiftGearCommand.java
 		}
 	}
 
