77654c6fe0ede4fe4a61f7c30870e9df8c4b2866
Peter Wu <ilikepie.wu307@gmail.com>
2017-02-15 19:23:38 -0500

    Fixed the DrivetrainSubsystem instance name and updated DrivetrainSubsystem with encoder methods (#22)
    * change driveSubsystem to driveSubSys
    
    * Update DrivetrainSubsystem with encoder methods

--- src/org/usfirst/frc/team1923/robot/commands/driveCommands/EncoderDriveCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/driveCommands/EncoderDriveCommand.java
@@ -19,8 +19,8 @@ public class EncoderDriveCommand extends Command {
 	 * @param distance distance to be traveled in inches
 	 * @param power speed to travel at
 	 */
-    public EncoderDriveCommand( double distance, double power ) {
-    	requires(Robot.driveSubSystem);
+    public EncoderDriveCommand(double distance, double power) {
+    	requires(Robot.driveSubSys);
     	leftDistance = distance;
     	rightDistance = distance;
 //    	speed = 0;
@@ -28,40 +28,40 @@ public class EncoderDriveCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.driveSubSystem.resetPosition();
-    	Robot.driveSubSystem.enableControl();
-    	Robot.driveSubSystem.drive(rightDistance, leftDistance, TalonControlMode.Position);
+    	Robot.driveSubSys.resetPosition();
+    	Robot.driveSubSys.enableControl();
+    	Robot.driveSubSys.drive(rightDistance, leftDistance, TalonControlMode.Position);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	SmartDashboard.putNumber("Left Encoder Pos: ", Robot.driveSubSystem.leftTalons[0].getEncPosition());
-    	SmartDashboard.putNumber("Right Encoder Pos: ", Robot.driveSubSystem.rightTalons[0].getEncPosition());
+    	SmartDashboard.putNumber("Left Encoder Pos: ", Robot.driveSubSys.leftTalons[0].getEncPosition());
+    	SmartDashboard.putNumber("Right Encoder Pos: ", Robot.driveSubSys.rightTalons[0].getEncPosition());
     	
-    	SmartDashboard.putNumber("Left Error Value", Robot.driveSubSystem.leftTalons[0].getError());
+    	SmartDashboard.putNumber("Left Error Value", Robot.driveSubSys.leftTalons[0].getError());
 
     	SmartDashboard.putNumber("Left Encoder Target ", leftDistance);
     	SmartDashboard.putNumber("Right Encoder Target ", rightDistance);
-    	SmartDashboard.putNumber("Right Get: ",Robot.driveSubSystem.rightTalons[0].get());
-    	SmartDashboard.putNumber("Left Get: ", Robot.driveSubSystem.leftTalons[0].get());
+    	SmartDashboard.putNumber("Right Get: ",Robot.driveSubSys.rightTalons[0].get());
+    	SmartDashboard.putNumber("Left Get: ", Robot.driveSubSys.leftTalons[0].get());
 
-    	SmartDashboard.putNumber("Left Motor Speed: ", Robot.driveSubSystem.leftTalons[0].getSpeed());
-    	SmartDashboard.putNumber("Right Motor Speed: ", Robot.driveSubSystem.rightTalons[0].getSpeed());
-    	SmartDashboard.putNumber("Right Encoder Position (getPosition): ",Robot.driveSubSystem.rightTalons[0].getPosition());
-     	SmartDashboard.putNumber("Left Encoder Position (getPosition): ",Robot.driveSubSystem.leftTalons[0].getPosition());
-    	SmartDashboard.putString("Control Mode", Robot.driveSubSystem.rightTalons[0].getControlMode().toString());
+    	SmartDashboard.putNumber("Left Motor Speed: ", Robot.driveSubSys.leftTalons[0].getSpeed());
+    	SmartDashboard.putNumber("Right Motor Speed: ", Robot.driveSubSys.rightTalons[0].getSpeed());
+    	SmartDashboard.putNumber("Right Encoder Position (getPosition): ",Robot.driveSubSys.rightTalons[0].getPosition());
+     	SmartDashboard.putNumber("Left Encoder Position (getPosition): ",Robot.driveSubSys.leftTalons[0].getPosition());
+    	SmartDashboard.putString("Control Mode", Robot.driveSubSys.rightTalons[0].getControlMode().toString());
 
   }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
     	return false;
-        //return (Robot.driveSubSystem.leftTalons[0].getError() < Robot.driveSubSystem.ENCODER_ERROR_MARGIN );
+        //return (Robot.driveSubSys.leftTalons[0].getError() < Robot.driveSubSystem.ENCODER_ERROR_MARGIN );
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveSubSystem.stop();
+    	Robot.driveSubSys.stop();
     }
 
     // Called when another command which requires one or more of the same
