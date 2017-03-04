067f4f3a397055d13b9c899995d69be79c40dc00
Tim Magoun <timmagoun@gmail.com>
2017-03-04 16:02:27 -0500

    Revert "Bringing this branch up to date with beta&master features"

--- src/org/usfirst/frc/team1923/robot/commands/driveCommands/TurnTimeCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/driveCommands/TurnTimeCommand.java
@@ -1,34 +1,43 @@
 package org.usfirst.frc.team1923.robot.commands.driveCommands;
 
+
 import org.usfirst.frc.team1923.robot.Robot;
-import com.ctre.CANTalon.TalonControlMode;
+
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Command;
 
-public class TurnTimeCommand extends Command {
-
+public class TurnTimeCommand extends Command{
+	
 	private double power;
 
-	public TurnTimeCommand(double power, double timeOut) {
+	public TurnTimeCommand(double power, double timeOut)
+	{
 		requires(Robot.driveSubSys);
 		this.power = power;
 		setTimeout(timeOut);
 	}
-
-	protected void initialize() {
-		Robot.driveSubSys.drive(power, -power, TalonControlMode.PercentVbus);
+	
+	protected void initialize()
+	{
+		Robot.driveSubSys.turnTime(power);;
 	}
-
-	protected void execute() {
-
+	
+	protected void execute()
+	{
+		
 	}
-
-	protected void end() {
+	
+	protected void end()
+	{
 		Robot.driveSubSys.stop();
 	}
-
-	protected void interrupted() {
+	
+	protected void interrupted()
+	{
 		end();
 	}
+	
 
 	@Override
 	protected boolean isFinished() {
