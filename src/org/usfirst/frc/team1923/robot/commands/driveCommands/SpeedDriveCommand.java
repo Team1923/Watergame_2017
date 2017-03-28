3d037a0c81231d1e94fc8bf953eebe37218cab46
Tim Magoun <timmagoun@gmail.com>
2017-03-28 12:31:19 -0400

    Revert "Vision formatted"

--- src/org/usfirst/frc/team1923/robot/commands/driveCommands/SpeedDriveCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/driveCommands/SpeedDriveCommand.java
@@ -1,41 +1,45 @@
-package org.usfirst.frc.team1923.robot.commands.visionCommands;
+package org.usfirst.frc.team1923.robot.commands.driveCommands;
 
 import org.usfirst.frc.team1923.robot.Robot;
+import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class VisionProcessing extends Command {
+public class SpeedDriveCommand extends Command {
 
-	public VisionProcessing() {
+	public static final int MAX_RPM = 1000; // aka 100% power
+
+	public SpeedDriveCommand() {
 		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		requires(Robot.visionSubSys);
+		requires(Robot.driveSubSys);
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		Robot.visionSubSys.refresh();
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.visionSubSys.refresh();
+		Robot.driveSubSys.drive(MAX_RPM * Robot.driveSubSys.dprofile.scale(Robot.oi.driver.getLeftY()),
+				MAX_RPM * Robot.driveSubSys.dprofile.scale(Robot.oi.driver.getRightY()), TalonControlMode.Speed);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return true;
+		return false;
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
+		Robot.driveSubSys.stop();
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
--- src/org/usfirst/frc/team1923/robot/commands/driveCommands/TurnTimeCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/driveCommands/TurnTimeCommand.java
@@ -1,26 +1,25 @@
-package org.usfirst.frc.team1923.robot.commands.visionCommands;
+package org.usfirst.frc.team1923.robot.commands.driveCommands;
 
 import org.usfirst.frc.team1923.robot.Robot;
 import com.ctre.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class VisionScanLeftCommand extends Command {
+public class TurnTimeCommand extends Command {
 
 	private double power;
 
-	public VisionScanLeftCommand(double power, double timeOut) {
+	public TurnTimeCommand(double power, double timeOut) {
 		requires(Robot.driveSubSys);
 		this.power = power;
 		setTimeout(timeOut);
 	}
 
 	protected void initialize() {
-		
+		Robot.driveSubSys.drive(power, -power, TalonControlMode.PercentVbus);
 	}
 
 	protected void execute() {
-		Robot.visionSubSys.refresh();
-		Robot.driveSubSys.drive(power, -power, TalonControlMode.PercentVbus);
+
 	}
 
 	protected void end() {
@@ -33,7 +32,7 @@ public class VisionScanLeftCommand extends Command {
 
 	@Override
 	protected boolean isFinished() {
-		return isTimedOut() || (Robot.visionSubSys.centerx>0 && Robot.visionSubSys.centerx<300);
+		return isTimedOut();
 	}
 
 }
